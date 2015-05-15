9cb50b34e99e3a40e23eaada5f35d81584a35c3e
Tiefeng Wu <icebergwtf@qq.com>
2015-05-15 13:03:21 +0800

    use-package setting

--- init.el
+++ init.el
@@ -9,38 +9,14 @@
 
 ;;; Code:
 
-(require 'package)
-
-(defvar is-mac (equal system-type 'darwin))
-(defvar is-mswin (equal system-type 'windows-nt))
-(defvar is-cygwin (equal system-type 'cygwin))
-
-(let ((archives
-       '(("melpa" . "http://melpa.org/packages/")
-         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-         ("marmalade" . "http://marmalade-repo.org/packages/"))))
-  (dolist (arch archives)
-    (add-to-list 'package-archives arch)))
-
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives
-               '("gnu" . "http://elpa.gnu.org/packages/")))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (unless (assoc 'use-package package-archive-contents)
-    (package-refresh-contents))
-  (package-install 'use-package))
-
-(require 'use-package)
-
-(let* ((current-dir (file-name-directory load-file-name))
-       (setup-dir (expand-file-name "setup" current-dir)))
+(let* ((load-dir (file-name-directory load-file-name))
+       (setup-dir (expand-file-name "setup" load-dir)))
   (add-to-list 'load-path setup-dir))
 
-(require 'evil-setup)
+(require 'core-setup)
+
 (require 'lisp-setup)
+
 ;(require 'minibuf-setup)
 ;(require 'flycheck-setup)
 ;(require 'acejump-setup)
@@ -48,14 +24,5 @@
 ;(require 'magit-setup)
 ;(require 'misc-setup)
 
-(global-set-key (kbd "M-h") 'previous-buffer)
-(global-set-key (kbd "M-l") 'next-buffer)
-
-(when window-system
-  ;; windows system frame default size
-  (add-to-list 'default-frame-alist '(height . 25))
-  (add-to-list 'default-frame-alist '(width . 100)))
-
-(message "MINE config load finished!")
 
 ;;; init.el ends here
--- setup/package-setup.el
+++ setup/package-setup.el
@@ -31,6 +31,7 @@
     (package-refresh-contents))
   (package-install 'use-package))
 
+(require 'use-package)
 (setq use-package-always-ensure t)
 
 (provide 'package-setup)
