4c00f3a703455fcd48ef4f51a5babc8bc727495a
Tiefeng Wu <icebergwtf@qq.com>
2016-02-16 18:35:48 +0800

    csharp-setup

--- setup/completion-setup.el
+++ setup/completion-setup.el
@@ -15,13 +15,12 @@
 (use-package company
   :diminish company-mode
   :config
-  (setq company-idle-delay 0.5)
-  (setq company-tooltip-limit 10)
+  (setq company-idle-delay 0.3)
+  (setq company-tooltip-limit 12)
   (setq company-minimum-prefix-length 2)
   ;; invert the navigation direction if the the completion popup-isearch-match
   ;; is displayed on top (happens near the bottom of windows)
   (setq company-tooltip-flip-when-above t)
-
   (global-company-mode 1))
 
 ;; ido
