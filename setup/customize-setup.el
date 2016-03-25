147583d94442072ecae3ca877c5c6c4961f634da
Tiefeng Wu <icebergwtf@qq.com>
2016-03-25 18:57:55 +0800

    update customize-setup.el

--- setup/customize-setup.el
+++ setup/customize-setup.el
@@ -12,19 +12,20 @@
 (require 'projectile-setup)
 (require 'syntax-check-setup)
 
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
-;;  '(projectile-tags-command "etags -Re -f \"%s\" %s"))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ ;;'(projectile-tags-command "etags -Re -f \"%s\" %s")
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (provide 'customize-setup)
 
