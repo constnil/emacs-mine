527a732ae43df1c65a35acff6cf064d1801f3186
Tiefeng Wu <icebergwtf@qq.com>
2016-04-01 12:56:08 +0800

    load mine config suit properly

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; .emacs --- The dot emacs file
+;;; init.el --- Emacs initial file
 
 ;; Copyright (C) 2015, Wu Tiefeng.
 
@@ -9,17 +9,15 @@
 
 ;;; Code:
 
-(defconst default-config "mine")
-(defvar config-suit-name (getenv "EMACS_CONFIG"))
+(defconst default-suit "mine")
+(defvar config-suit (getenv "EMACS_CONFIG"))
 (let ((config-list '("prelude" "live" "space" "mine")))
-  (unless (member config-suit-name config-list)
+  (unless (member config-suit config-list)
     (message "$EMACS_CONFIG not specified, use default: %s"
-             default-config)
-    (setq config-suit-name default-config)))
-
-(setq user-emacs-directory
-      (expand-file-name (format "~/.emacs.d/%s/" config-suit-name)))
-(add-to-list 'load-path user-emacs-directory)
+             default-suit)
+    (setq config-suit default-suit)))
+(defvar config-root (expand-file-name config-suit user-emacs-directory))
+(add-to-list 'load-path config-root)
 
 (require 'server)
 (setq server-auth-dir
@@ -30,10 +28,8 @@
 (setq package-user-dir
       (expand-file-name "elpa" user-emacs-directory))
 
-(message "load emacs using configuration: \"%s\" (%s) - begin"
-         config-suit-name user-emacs-directory)
-(load (expand-file-name "init.el" user-emacs-directory))
-(message "load emacs using configuration: \"%s\" - end"
-         config-suit-name)
+(message "config suit \"%s\" start loading" config-suit)
+(load (expand-file-name "init.el" config-root))
+(message "config suit \"%s\" loaded" config-suit)
 
-;;; .emacs ends here
+;;; init.el ends here
