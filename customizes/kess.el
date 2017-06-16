fa4609f0b6e058be393372ab4d429bfc6f63c2a0
Tiefeng Wu <icebergwtf@qq.com>
2017-06-16 19:16:52 +0800

    kess: customize file kess.el ignore tasks.org in kess switch

--- customizes/kess.el
+++ customizes/kess.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages)))
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(kess-switch-ignore-regexs
+   (quote
+    ("\\*Ibuffer\\*" "\\*Messages\\*" "\\*scratch\\*" "tasks.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
